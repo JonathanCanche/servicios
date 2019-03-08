776c378b0ec3a42460277f9fcc892971da6061d8
jonathan canche <jhonathancanchem@gmail.com>
2019-03-08 11:32:42 -0600

    preproduccion

---
+++ src/app/interface.ts
@@ -0,0 +1,51 @@
+export interface WebData{
+
+    icon:string;
+    tipo:number;
+    secciones:{
+
+        home:{
+            navbar:string;
+            logo:string;
+            imagen:string;
+            titulo:string;
+        }
+
+        about:{
+            navbar:string;
+            titulo:string;
+            descripcion:string;
+            imagen:string;
+            Sdescripcion:string;
+            caracteristicas:Array<{imagen:string,titulo:string,descripcion:string}>;
+            nosotros:Array<{imagen:string,titulo:string,descripcion:string}>;
+        }
+
+        servicios:{
+            navbar:string;
+            titulo:string;
+            descripcion:string;
+            servicio:Array<{imagen:string,titulo:string,descripcion:string}>;
+        }
+
+        banner:{
+            titulo:string;
+            descripcion:string;
+            cuadroInfo:Array<{imagen:string,titulo:string,descripcion:string}>;
+        }
+
+        portafolio:{
+            navbar:string;
+            titulo:string;
+            imagenes:Array<{imagen:string,titulo:string,subtitulo:string}>;
+        }
+
+        contacto:{
+            navbar:string;
+            titulo:string;
+            telefono:string;
+            email:string;
+            lugar:string;
+        }
+    }
+}
