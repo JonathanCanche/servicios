e249c87b91217e8d876765e62435e612ad3fe965
jonathan canche <jhonathancanchem@gmail.com>
2019-03-20 15:19:45 -0600

    con notfound

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ElementRef, ViewChild } from '@angular/core';
 import{WebData} from 'src/app/interface'
 
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
@@ -6,10 +6,12 @@ import { Observable } from 'rxjs';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
+  
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
+
 export class LandingComponent implements OnInit {
 
   data:WebData;
@@ -18,10 +20,17 @@ export class LandingComponent implements OnInit {
   private itemDoc: AngularFirestoreDocument<WebData>;
   item: Observable<any>;
 
+    @ViewChild('home_li') homeLi: ElementRef;
+    @ViewChild('servicios_li') serviciosLi: ElementRef;
+    @ViewChild('about_li') aboutLi: ElementRef;
+    @ViewChild('contact_li') contactLi: ElementRef;
+    @ViewChild('portafolio_li') portafolioli: ElementRef;
+    
   constructor(private afs: AngularFirestore,private router: Router, private routes: ActivatedRoute) { 
 
     this.itemDoc=afs.doc<WebData>('webs/pruebaservicios');
     this.item=this.itemDoc.valueChanges();
+
 
    // this.cargardatos(this.data);
   }
@@ -36,7 +45,6 @@ export class LandingComponent implements OnInit {
       this.itemDoc = this.afs.doc<any>(`webs/${id}`);
       this.item = this.itemDoc.valueChanges();
       this.item.subscribe((data) => {
-        console.log('data', data);
         if (data === undefined || data.tipo != 3) {
           this.router.navigateByUrl('/404');
         } else {
@@ -47,8 +55,57 @@ export class LandingComponent implements OnInit {
     });
   }
 
-  scrollToElement($element): void {
+  scrollToElement($element, activeElement: string): void {
+    // this.buttonmenu.nativeElement.click();
     $element.scrollIntoView({behavior: 'smooth', block: 'start'});
-  }
+    this.activesToogle(activeElement);
+  } 
 
+  activesToogle( el: string) {
+    switch (el) {
+      case 'home':
+        this.homeLi.nativeElement.classList.add('active');
+        this.serviciosLi.nativeElement.classList.remove('active');
+        this.aboutLi.nativeElement.classList.remove('active');
+        this.contactLi.nativeElement.classList.remove('active');
+        this.portafolioli.nativeElement.classList.remove('active');
+        break;
+      case 'servicios':
+        this.homeLi.nativeElement.classList.remove('active');
+        this.serviciosLi.nativeElement.classList.add('active');
+        this.aboutLi.nativeElement.classList.remove('active');
+        this.contactLi.nativeElement.classList.remove('active');
+        this.portafolioli.nativeElement.classList.remove('active');
+        break;
+      case 'about':
+        this.homeLi.nativeElement.classList.remove('active');
+        this.serviciosLi.nativeElement.classList.remove('active');
+        this.aboutLi.nativeElement.classList.add('active');
+        this.contactLi.nativeElement.classList.remove('active');
+        this.portafolioli.nativeElement.classList.remove('active');
+        break;
+      case 'contact':
+        this.homeLi.nativeElement.classList.remove('active');
+        this.serviciosLi.nativeElement.classList.remove('active');
+        this.aboutLi.nativeElement.classList.remove('active');
+        this.contactLi.nativeElement.classList.add('active');
+        this.portafolioli.nativeElement.classList.remove('active');
+        break;
+        case 'portafolio':
+        this.homeLi.nativeElement.classList.remove('active');
+        this.serviciosLi.nativeElement.classList.remove('active');
+        this.aboutLi.nativeElement.classList.remove('active');
+        this.contactLi.nativeElement.classList.remove('active');
+        this.portafolioli.nativeElement.classList.add('active');
+        break;
+      default:
+        this.homeLi.nativeElement.classList.add('active');
+        this.serviciosLi.nativeElement.classList.remove('active');
+        this.aboutLi.nativeElement.classList.remove('active');
+        this.contactLi.nativeElement.classList.remove('active');
+        this.portafolioli.nativeElement.classList.remove('active');
+        break;
+    }
+  }
+  
 }
